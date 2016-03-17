338ca64b456781366557a6f2b5f1188111981320
LemonAtSu <at_7788546@hotmail.com>
2016-03-18 01:58:54 +0800

    Update some details

--- Q3.py
+++ Q3.py
@@ -46,14 +46,48 @@ def reshape(data):
     
     return list
 
+def writeCSV(dataset):
+    div = len(dataset) / 4 # 4 kinds of instrument
+    featPath = './features/'
+    
+    for i in range(0, len(dataset)):
+        if i < div :
+            fn = 'guitar/'
+        elif i < div * 2:
+            fn = 'piano/'
+        elif i < div * 3:
+            fn = 'violin/'
+        else:
+            fn = 'voice/'
+        fn = featPath + fn + '%03d' % ((i % div) + 1)
+        numpy.savetxt(fn + '.csv', dataset[i])
+
+def readCSV():
+    featGuitar = './features/guitar/'
+    featPiano = './features/piano/'
+    featViolin = './features/violin/'
+    featVoice = './features/voice/'
+    featDirList = [featGuitar, featPiano, featViolin, featVoice]
+
+    features_set = []    
+
+    for p in featDirList:
+        for file in os.listdir(p):
+            if file.endswith('.csv'):
+                data = numpy.genfromtxt(p + file) 
+                features_set.append(data)
+    return features_set
+
 if __name__ == '__main__':
-    w = 1024
-    h = 512
+    start = time.clock()
+    w = 4096
+    h = 2048
     
-    
-    if len(sys.argv) >= 2 and sys.argv[1]:
+    if len(sys.argv) >= 2 and sys.argv[1] == 'r':
+        print('read from .csv ..')
         features_set = readCSV()
     else :
+        print('read from .wav ..')
         list_guitar = listFile('../audio/guitar/')   
         list_piano  = listFile('../audio/piano/')
         list_violin = listFile('../audio/violin/')
@@ -66,8 +100,9 @@ if __name__ == '__main__':
             for song in songs:
                 x = extractMFCC(song[0], song[1], w, h)
                 features_set.append(x)
-        #writeCSV(features_set)
+        writeCSV(features_set)
     
+    print('finish loading features.')
     # count the number of training and validation set
     ans = [(i/(len(features_set)/4)+ 1) for i in range(0, 800)]
     nValidation = len(features_set) / 5
@@ -103,4 +138,7 @@ if __name__ == '__main__':
 
     XTrain = reshape(XTrain)
     XValidation = reshape(XValidation)
+    print('start training ...')
     trainModel(XTrain, YTrain, XValidation, YValidation)
+    print('total time elapsed : %f' %(time.clock() - start))
+
