048e8e3b142185c75b3ba3b66a18b876076d5319
LemonAtSu <at_7788546@hotmail.com>
2016-03-24 00:59:47 +0800

    Now will return top 5 clf

--- train.py
+++ train.py
@@ -2,36 +2,51 @@ import numpy, librosa
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
+from sklearn.qda import QDA
+
 
 def trainSVMModel(XTrain, YTrain, XValid, YValid):
-    Cs = [1, 10, 100, 200, 400, 500, 700, 1000]
-    g0 = 1 / float(len(XTrain))
+    Cs = [.1, 1, 10, 100, 500, 1000]
+    g0 = 1 / float(len(XTrain[0]))
     Gs = [g0, g0 / 10, g0 / 50, g0 / 70, g0 / 100]
     kernel = ['linear', 'poly', 'rbf', 'sigmoid']
-    best_accuracy = 0.0
+    #best_accuracy = 0.0
+    clfs = []
 
     for c in range(0, len(Cs)):
         for g in range(0, len(Gs)):
-            clf = svm.SVC(C=Cs[c],  gamma=Gs[g], kernel=kernel[2])            
+            clf = svm.SVC(C=Cs[c], probability=True,  gamma=Gs[g], kernel=kernel[2])            
             clf.fit(XTrain, YTrain)
             score = clf.score(XValid, YValid)
+            print("score : %f, c : %f, g : %f" % (score, Cs[c], Gs[g]))
+            
+            info = {'clf' : clf, 'score' : score, 'c' : Cs[c], 'g' : Gs[g]}
+            clfs.append(info)
+            """
             if score > best_accuracy:
                 best_accuracy = score
                 best_model   = clf
                 bestC = Cs[c]
                 bestG = Gs[g]
+            """
 
-    print("best score : %f, Cs : %f, Gs : %f" % (best_accuracy, bestC, bestG))
+    # print("best score : %f, Cs : %f, Gs : %f" % (best_accuracy, bestC, bestG))
 
-    XPred = best_model.predict(XValid);
-    CTable = confusion_matrix(YValid, XPred)
+    #XPred = best_model.predict(XValid);
+    #CTable = confusion_matrix(YValid, XPred)
 
-    print(CTable)
+    #print(CTable)
 
-    return best_model, best_accuracy, bestC, bestG
+    clfs = sorted(clfs, key=lambda k : k['score'], reverse=True)
+
+    return clfs[0:5]
 
 def trainKNNModel(XTrain, YTrain, XValid, YValid):
     nbrs = KNeighborsClassifier(n_neighbors=4, weights='distance').fit(XTrain, YTrain)
     print ("score : %f" % (nbrs.score(XValid, YValid)))
 
 
+def trainQDA(XTrain, YTrain, XValid, YValid):
+    qda = QDA()
+    qda.fit(XTrain, YTrain)    
+    print('QDA score : %f' % (qda.score(XValid, YValid)))
